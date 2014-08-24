76b7c6e2bfc435a78c1c4ee9ac4a7258c55e4518
Chris Grimmett <chris@grimtech.net>
2014-08-24 01:24:15 -0700

    added texture atlas for player

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ ld30
 ====
 
 Connected Worlds Ludem Dare 30 http://www.ludumdare.com/compo/
+
+Team RPC
+
+
+
+Notes
+-----
+
+
+* Sprite
+** Player
+*** Controllable
+
+
+
+* TilingSprite
+** Far
+** Mid
+
--- client/js/ld30.js
+++ client/js/ld30.js
@@ -1,81 +1,89 @@
 var moveSpeed = 3;
 
+console.log('creating stuff');
 
-var stage = new PIXI.Stage(0x66FF99);
-var renderer = new PIXI.autoDetectRenderer(
-    640,
-    480
-);
-var loader = new PIXI.AssetLoader(
-    [
-	'/img/test.png',
-	'/img/characters.json'
-    ],
-    false);
-
-loader.addEventListener('onLoaded', function(e) {
-    console.log('e: onLoaded: All files loaded');
-});
-
-loader.addEventListener('onProgress', function(e) {
-    console.log('e: onProgress: file loading in progress');
-});
-
-loader.addEventListener('onComplete', function(e) {
-    console.log('e: onComplete: all assets hav loaded');
-});
-
-loader.onComplete = onAssetsLoaded;
-
-loader.load();
+var Q = window.Q = Quintus()
+    .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
+    .setup({ maximize: true })
+    .controls().touch();
 
 
-function onAssetsLoaded() {
-
-    console.log('creating stuff');
-
-    var characterAtlas = PIXI.AtlasLoader('/img/characters.json', false);
-    var player = PIXI.Texture.fromFrame('player.png');
-    var test = PIXI.Texture.fromFrame('test.png');
-
-    testguy = new PIXI.Sprite(player);
-
-    testguy.position.x = 50;
-    testguy.position.y = 50;
-    
-    stage.addChild(testguy);
-    
-    // add background
-    // add ground
-    // add structures
-    // add knight
-    // add robot
-
-    renderer.render(stage);
-    
-    
-}
-
-
-function updateScreen() {
-    renderer.render(stage);
-    
-}    
-
-
-document.addEventListener('keydown', function(event) {
-    if(event.keyCode == 37) {
-	console.log('left was pressed');
-	testguy.position.x -= moveSpeed;
-	updateScreen();
-    }
-    else if(event.keyCode == 39) {
-	console.log('right was pressed');
-	testguy.position.x += moveSpeed;
-	updateScreen();	
+/**
+ * Player. Extends sprite.
+ */
+Q.Sprite.extend("Player", {
+    init: function(p) {
+	this._super(p, {
+	    // sheet is the frame from used from the atlas
+	    sheet: "",
+	    x: 64,
+	    y: 64
+	});
+	
+	this.add('2d, platformerControls');
+	
+	this.on("hit.sprite", function(collision) {
+	    if(collision.obj.isA("Opponent")) {
+		
+		Q.stageScene("info", 1, {
+		    label: "you collided with opponent"
+		});
+		
+	    }
+	});
     }
 });
 
-document.body.appendChild(renderer.view);
+
+Q.Sprite.extend("Opponent", {
+    init: function(p) {
+	this._super(p, { sheet: 'test' });
+    }
+});
 
 
+
+
+/**
+ * Scene - Battlefield
+ */
+Q.scene("portalBattlefield", function(stage) {
+    
+    // tiles for the ground of this map
+    stage.collisionLayer(new Q.TileLayer({
+	dataAsset: 'portalBattlefield.json',
+	sheet: 'test' }));
+    
+    // create the player
+    var player = stage.insert(new Q.Player());
+    
+    stage.insert(new Q.Opponent({ x: 200, y: 100 }));
+    stage.insert(new Q.Opponent({ x: 300, y: 200 }));
+    
+});
+
+Q.scene('info', function(stage) {
+    var container = stage.insert(new Q.UI.Container({
+	x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+    }));
+    
+    var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#00FF00",
+						    label: "DERP" }));
+    var label = container.insert(new Q.UI.Text({ x: 10, y: -10 -button.p.h,
+						 label: stage.options.label }));
+    
+    container.fit(20);
+});
+
+// load assets
+Q.load("/img/characters.png", "/img/characters.json", "/img/portalBattlefield.json", "/img/tiles.png", function() {
+
+    
+    Q.sheet("tiles", "/img/tiles.png", { tilew: 32, tileh: 32 });
+    
+    // when assets loaded, compile sprites from atlas
+    Q.compileSheets("chracters.png", "characters.json");
+    
+    // start scene
+    Q.stageScene("portalBattlefield");
+});
